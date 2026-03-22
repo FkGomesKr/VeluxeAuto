d75f7dc17e3d2dd28f1e8d00db240c18aa0e6f70
Pedro Gomes <pedro.gomes2@fanduel.com>
2026-03-22 03:47:28 +0000

    full dark

--- app.vue
+++ app.vue
@@ -23,11 +23,10 @@ useHead({
   background-color: #d47474;
   color: white; 
 }
+
 html, body {
-  background: linear-gradient(to bottom, #121212 50%, #201818 100%);
-  background-attachment: fixed;
+  background-color: #121212;
   font-family: 'Nunito Sans', sans-serif;
   color: #121212;
-  overflow-x: hidden;
 }
 </style>
